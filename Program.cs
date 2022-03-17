9cf6a81bae3fdec9cde49e98ae0c12a15c8f576e
ofuremomoh <95165710+ofuremomoh@users.noreply.github.com>
2022-03-17 10:00:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class AreaOfRectangle
+{
+    public static void Main()
+    {
+        int Length = 15;
+        int Breadth = 6;
+        int Area = Length * Breadth;
+        Console.WriteLine($"Area of Length {Length} and Breadth {Breadth} Rectangle is {Area}");
+        Console.ReadLine();
+    }
+}
+
