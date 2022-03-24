68f028e9ff8ab33c1abadf92b3533e3160e378f5
ofuremomoh <95165710+ofuremomoh@users.noreply.github.com>
2022-03-24 10:07:40 +0100

    Add files via upload

---
+++ conditional.cs
@@ -0,0 +1,29 @@
+﻿namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "3782";
+            var b = Convert.ToInt32(a);
+
+            double c = b / 100;
+
+            var x = c % 10;
+            var y = a[1];
+
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.WriteLine(x == 7 && y == 7 ? "You are correct" : "You are wrong");
+
+
+        }
+
+    }
+
+} 
